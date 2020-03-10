10244af698536935cb6401557a103c713d786187
Yacinesaf <yacinesaf96@gmail.com>
2020-03-09 20:01:15 -0400

    first day of mui component training : skeletons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from '@material-ui/core/Card';
+
+import Skeleton from '@material-ui/lab/Skeleton';
+
+
+
 
 function App() {
+  const loading = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card style={{height : 400, width : 400}}>
+        {loading ?
+            <Skeleton animation="wave" height='100%' />
+        : null}
+    </Card>
   );
 }
 
