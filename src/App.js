e6e6840d0f4b5398d87e56fb079fa352e26b72be
Yacinesaf <yacinesaf96@gmail.com>
2020-03-09 22:42:59 -0400

    the mouseover event never works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import Card from '@material-ui/core/Card';
-
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
 import Skeleton from '@material-ui/lab/Skeleton';
 
 
@@ -9,13 +10,50 @@ import Skeleton from '@material-ui/lab/Skeleton';
 
 function App() {
   const loading = true
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handlePopoverOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
   return (
-    <Card style={{height : 400, width : 400}}>
+    <div>
+      <Card style={{ height: 400, width: 400 }}>
         {loading ?
-            <Skeleton animation="wave" height='100%' />
-        : null}
-    </Card>
+          <Skeleton variant='text' animation="wave" height='100%' />
+          : null}
+      </Card>
+      <Typography
+        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      >
+        Hover with a Popover.
+  </Typography>
+      <Popover
+        id="mouse-over-popover"
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        <Typography>I use Popover.</Typography>
+      </Popover>
+    </div>
   );
 }
 
